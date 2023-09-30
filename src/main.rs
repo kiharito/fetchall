1abc561bc7113c319dca248c22972f1228167107
kiharito <kihara13611.c7d7@gmail.com>
2023-09-30 22:17:56 +0900

    コマンドの大枠完成

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
+#[command(propagate_version = false)]
 struct Cli {
-    name: Option<String>,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Add a directory path to the targets
+    Add { path: String },
+    /// Remove the specified directory path from the targets
+    Rm { index: u32 },
+    /// Show the list of the target directory paths
+    Ls {},
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    match cli.name {
-        Some(name) => println!("Name: {}", name),
-        None => println!("No Name..."),
+    match cli.command {
+        Some(Commands::Add { path }) => {
+            println!("Added: {}", path);
+        }
+        Some(Commands::Rm { index }) => {
+            println!("Removed: {}", index);
+        }
+        Some(Commands::Ls {}) => {
+            println!("ls!");
+        }
+        None => {
+            println!("fetch all!!")
+        }
     }
 }
