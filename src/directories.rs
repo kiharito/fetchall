748fc24f16ce60ce6379fa2a3416a8bc597a6b70
kiharito <kihara13611.c7d7@gmail.com>
2023-10-10 23:30:57 +0900

    addの書き込み先ファイルをjson化

--- src/directories.rs
+++ src/directories.rs
@@ -1,8 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::fs::OpenOptions;
-use std::hash::Hasher;
-use std::io::{Read, Result, Write};
+use std::io::Result;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Directory {
@@ -10,19 +9,22 @@ struct Directory {
 }
 
 pub fn add_directory_path(file_path: &str, path: String) -> Result<()> {
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .write(true)
         .create(true)
         .read(true)
         .open(file_path)?;
-    let mut dirs: Vec<Directory> = serde_json::from_reader(file)?;
+    let mut dirs: Vec<Directory> = match serde_json::from_reader(file) {
+        Ok(dirs) => dirs,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
     dirs.push(Directory { path });
-    let dirs_str = serde_json::to_string(&dirs).unwrap();
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .write(true)
         .create(true)
         .read(true)
         .open(file_path)?;
-    serde_json::to_writer(file, &dirs_str)?;
+    serde_json::to_writer(file, &dirs)?;
     Ok(())
 }
