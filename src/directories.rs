a51bd255ccad324b85dd3213a8aa634a87bacc9c
kiharito <kihara13611.c7d7@gmail.com>
2023-10-26 23:03:02 +0900

    test追加準備

--- src/directories.rs
+++ src/directories.rs
@@ -72,3 +72,11 @@ pub fn fetchall(repo: &impl Repository) -> Result<()> {
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
